0244acdedbb2ff9b97b109265f8beee0aec571d1
Brandon Williams <mbw234@gmail.com>
2017-08-14 16:36:23 -0500

    More css

--- Sources/Css/Text.swift
+++ Sources/Css/Text.swift
@@ -58,3 +58,26 @@ extension TextAlign: ExpressibleByStringLiteral {
 }
 
 public let textAlign: (TextAlign) -> Stylesheet = key("text-align")
+
+public func letterSpacing(_ size: Css.Size) -> Stylesheet {
+  return key("letter-spacing")(size)
+}
+
+public struct TextTransform: Val, None, Inherit {
+  let transform: Css.Value
+
+  public func value() -> Css.Value {
+    return self.transform
+  }
+
+  public static let none = TextTransform(transform: .none)
+  public static let inherit = TextTransform(transform: .inherit)
+  public static let capitalize = TextTransform(transform: "capitalize")
+  public static let uppercase = TextTransform(transform: "uppercase")
+  public static let lowercase = TextTransform(transform: "lowercase")
+  public static let fullWidth = TextTransform(transform: "full-width")
+}
+
+public func textTransform(_ transform: TextTransform) -> Stylesheet {
+  return key("text-transform")(transform)
+}
