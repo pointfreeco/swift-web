3616ba2f6c56cdea941a07356a6cb54dc389bb90
Brandon Williams <mbw234@gmail.com>
2017-09-11 17:53:22 -0400

    Support for flexbox

---
+++ Tests/CssTests/FlexBoxTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import Prelude
+import Css
+import CssTestSupport
+import SnapshotTesting
+
+class FlexBoxTests: XCTestCase {
+
+  func testFlexBox() {
+    let css: Stylesheet =
+      ".wrapper" % (
+        display(.flex)
+          <> flex(direction: .row, wrap: .wrap)
+          <> .star % (
+            flex(grow: 1, basis: .pct(100))
+          )
+        )
+        <> ".main" % (
+          flex(grow: 1, basis: .auto)
+            <> align(items: .center)
+            <> justify(content: .center)
+
+        )
+        <> ".aside-1" % order(1)
+        <> ".aside-2" % order(2)
+        <> ".aside-3" % order(3)
+
+    assertSnapshot(matching: css)
+  }
+}
