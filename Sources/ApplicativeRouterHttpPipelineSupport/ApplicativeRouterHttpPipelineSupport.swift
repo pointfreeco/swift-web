f506deb58f116c30c5c00b11b3a1f1507e2ed34b
Brandon Williams <mbw234@gmail.com>
2017-09-26 12:35:29 +0200

    docs

--- Sources/ApplicativeRouterHttpPipelineSupport/ApplicativeRouterHttpPipelineSupport.swift
+++ Sources/ApplicativeRouterHttpPipelineSupport/ApplicativeRouterHttpPipelineSupport.swift
@@ -3,6 +3,12 @@ import Foundation
 import HttpPipeline
 import Prelude
 
+/// Middleware transformer that performs routing via a router, and if a route is unrecognized runs the
+/// `notFound` middleware supplied.
+///
+/// - Parameters:
+///   - router: A router from `ApplicativeRouter`.
+///   - notFound: Middleware to run if a route is not recognized.
 public func route<I, A, Route>(
   router: Parser<I, Route>,
   notFound: @escaping Middleware<StatusLineOpen, ResponseEnded, A, Data?> = notFound(respond(text: "don't know that url"))
