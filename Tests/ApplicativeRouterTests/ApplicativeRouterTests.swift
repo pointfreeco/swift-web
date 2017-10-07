fb231b4518bf648ce4871d2e2448bae566e7e10c
Brandon Williams <mbw234@gmail.com>
2017-10-07 11:29:21 +0200

    Added a bunch of tests for router.

--- Tests/ApplicativeRouterTests/ApplicativeRouterTests.swift
+++ Tests/ApplicativeRouterTests/ApplicativeRouterTests.swift
@@ -12,21 +12,57 @@ func req(_ method: ApplicativeRouter.Method, _ location: String, _ body: Data? =
 
 class ApplicativeRouterTests: XCTestCase {
   func testRouter() {
+
     let router =
       Route.home <¢ .get <*| end
-        <|> Route.episode <¢> (.get <* lit("episode") *> .str) <*| end
-        <|> Route.episodes <¢ (.get <* lit("episodes")) <*| end
-        <|> Route.search <¢> (.get <* lit("search") *> opt(param("query"))) <*| end
-        <|> Route.signup <¢> (.post *> .dataBody) <* lit("signup") <*| end
+        // Route with path argument and two optional query params: /episode/:id?ref=foo&t=123
+        <|> curry(Route.episode) <¢> (.get <* lit("episode") *> .str) <*> opt(param("ref")) <*> opt(param("t", .int)) <*| end
+
+        // Route with no path arguments and two query params, `page` and `ref`. However, the `ref` param
+        // is parsed using the `refTag` helper that will provide a first class wrapper type for that value.
+        <|> Route.episodes <¢> refTag(.get <* lit("episodes") *> opt(param("page", .int))) <*| end
+
+        // Route with one query param: /search?query=foo
+        <|> Route.search <¢ .get <* lit("search") <*> opt(param("query")) <*| end
+
+        // POST route with body data and an optional query param.
+        <|> curry(Route.signup) <¢> (.post *> .dataBody) <* lit("signup") <*> opt(param("ref")) <*| end
 
     XCTAssertEqual(router.match(req(.get, "/")), .home)
-    XCTAssertEqual(router.match(req(.get, "/episodes")), .episodes)
-    XCTAssertEqual(router.match(req(.get, "/episodes/")), .episodes)
-    XCTAssertEqual(router.match(req(.get, "/episodes//")), .episodes)
-    XCTAssertEqual(router.match(req(.get, "/episode/hello-world")), .episode("hello-world"))
+    XCTAssertEqual(router.match(req(.get, "/episodes")), Route.episodes(RefTag(ref: nil, rest: nil)))
+    XCTAssertEqual(router.match(req(.get, "/episodes/")), Route.episodes(RefTag(ref: nil, rest: nil)))
+    XCTAssertEqual(router.match(req(.get, "/episodes//")), Route.episodes(RefTag(ref: nil, rest: nil)))
+    XCTAssertEqual(
+      router.match(req(.get, "/episodes?ref=home")),
+      .episodes(RefTag(ref: "home", rest: nil))
+    )
+    XCTAssertEqual(
+      router.match(req(.get, "/episodes?ref=home&page=2")),
+      .episodes(RefTag(ref: "home", rest: 2))
+    )
+    XCTAssertEqual(
+      router.match(req(.get, "/episode/hello-world")),
+      .episode(id: "hello-world", ref: nil, startAt: nil)
+    )
+    XCTAssertEqual(
+      router.match(req(.get, "/episode/hello-world?ref=twitter")),
+      .episode(id: "hello-world", ref: "twitter", startAt: nil)
+    )
+    XCTAssertEqual(
+      router.match(req(.get, "/episode/hello-world?ref=twitter&t=1234")),
+      .episode(id: "hello-world", ref: "twitter", startAt: 1234)
+    )
     XCTAssertEqual(router.match(req(.get, "/search")), .search(nil))
     XCTAssertEqual(router.match(req(.get, "/search?query=what")), .search("what"))
     XCTAssertNil(router.match(req(.get, "/not-found")))
+    XCTAssertEqual(
+      .signup(Data("email=hi@pointfree.co".utf8), ref: nil),
+      router.match(req(.post, "/signup", Data("email=hi@pointfree.co".utf8)))
+    )
+    XCTAssertEqual(
+      .signup(Data("email=hi@pointfree.co".utf8), ref: "subscribe"),
+      router.match(req(.post, "/signup?ref=subscribe", Data("email=hi@pointfree.co".utf8)))
+    )
   }
 
   func testPostData() {
@@ -110,10 +146,10 @@ class ApplicativeRouterTests: XCTestCase {
 
 enum Route {
   case home
-  case episodes
-  case episode(String)
+  case episodes(RefTag<Int?>)
+  case episode(id: String, ref: String?, startAt: Int?)
   case search(String?)
-  case signup(Data)
+  case signup(Data, ref: String?)
 }
 
 extension Route: Equatable {
@@ -121,13 +157,15 @@ extension Route: Equatable {
     switch (lhs, rhs) {
     case (.home, .home):
       return true
-    case (.episodes, .episodes):
-      return true
-    case let (.episode(l), .episode(r)):
-      return l == r
+    case let (.episodes(lhs), .episodes(rhs)):
+      return lhs.ref == rhs.ref && lhs.rest == rhs.rest
+    case let (.episode(lhsId, lhsRef, lhsStartAt), .episode(rhsId, rhsRef, rhsStartAt)):
+      return lhsId == rhsId && lhsRef == rhsRef && lhsStartAt == rhsStartAt
     case let (.search(l), .search(r)):
       return l == r
-    default:
+    case let (.signup(lhsData, lhsRef), .signup(rhsData, rhsRef)):
+      return lhsData == rhsData && lhsRef == rhsRef
+    case (.home, _), (.episodes, _), (.episode, _), (.search, _), (.signup, _):
       return false
     }
   }
@@ -160,6 +198,28 @@ enum PostTestRoute: Equatable {
       return lhs == rhs
     default:
       return false
+    }
+  }
+}
+
+/// A struct that holds a ref tag that can be plucked from a query string, along with whatever else
+/// could get plucked from the request.
+struct RefTag<A> {
+  let ref: String?
+  let rest: A
+}
+
+/// Converts a parser of `A` into a parser of `RefTag<A>` by first trying to parse out the `A`, and then
+/// parsing a query param `ref` and storing both values in `RefTag<A>`.
+func refTag<I, A>(_ parser: Parser<I, A>) -> Parser<I, RefTag<A>> {
+  let refParser: Parser<I, String?> = opt(param("ref"))
+
+  return parser.flatMap { a in
+    Parser<I, RefTag<A>> { route in
+
+      refParser.parse(route)
+        .map { result in (result.rest, RefTag(ref: result.match, rest: a)) }
+        ?? (route, RefTag(ref: nil, rest: a))
     }
   }
 }
