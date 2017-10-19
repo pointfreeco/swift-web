91950b3b0e089deff7ababc102590c37e5e0cf2d
Stephen Celis <stephen@stephencelis.com>
2017-10-19 08:31:37 -0400

    Remove TODO

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     .library(name: "MediaType", targets: ["MediaType"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("aab186e")),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("6a292e9")),
+    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("a8a4849")),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("b4081b7")),
     .package(url: "https://github.com/bkase/DoctorPretty.git", .exact("0.3.0")),
     .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .exact("0.8.17"))
   ],
