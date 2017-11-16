15ff68a3c103b08e99919c56f29c72d2654deac7
Stephen Celis <stephen.celis@gmail.com>
2017-11-15 23:43:49 -0500

    Bump Prelude (#75)

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     .library(name: "MediaType", targets: ["MediaType"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("338f4b0")),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("d9fdff4")),
+    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("e341975")),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("d9e4781")),
     .package(url: "https://github.com/bkase/DoctorPretty.git", .exact("0.3.0")),
     .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .exact("0.8.21"))
   ],
