507071ffc7d944f1bf763ca9985436b1ca447f9b
Brandon Williams <mbw234@gmail.com>
2017-12-02 12:16:35 -0500

    fix

--- Sources/HttpPipelineHtmlSupport/Support.swift
+++ Sources/HttpPipelineHtmlSupport/Support.swift
@@ -4,5 +4,7 @@ import HttpPipeline
 import Prelude
 
 public func respond<A>(_ view: View<A>) -> Middleware<HeadersOpen, ResponseEnded, A, Data> {
-  return respond(body: view.rendered(with: $0), contentType: .html)
+  return { conn in
+    conn |> respond(body: view.rendered(with: conn.data), contentType: .html)
+  }
 }
