adf600b51ea02a3968316e4f249bc913adf6e994
Brandon Williams <mbw234@gmail.com>
2017-12-02 12:15:08 -0500

    Better way to do respond(view)

--- Sources/HttpPipelineHtmlSupport/Support.swift
+++ Sources/HttpPipelineHtmlSupport/Support.swift
@@ -4,10 +4,5 @@ import HttpPipeline
 import Prelude
 
 public func respond<A>(_ view: View<A>) -> Middleware<HeadersOpen, ResponseEnded, A, Data> {
-  
-  return
-    map { Data(view.rendered(with: $0).utf8) }
-      >>> writeHeader(.contentType(.html))
-      >-> closeHeaders
-      >-> end
+  return respond(body: view.rendered(with: $0), contentType: .html)
 }
