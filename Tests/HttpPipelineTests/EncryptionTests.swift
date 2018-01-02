e084097c00944839fa2b1f91c4c9daef0844f6f8
Brandon Williams <mbw234@gmail.com>
2018-01-02 10:43:45 -0500

    comments

--- Tests/HttpPipelineTests/EncryptionTests.swift
+++ Tests/HttpPipelineTests/EncryptionTests.swift
@@ -6,16 +6,29 @@ class EncryptionTests: XCTestCase {
   func testEncrypt() {
     XCTAssertEqual("af54a6cf18a83a6dc0a38e2895dd1ce4", encrypted(text: "blah", secret: "DeadBeefDeadBeef0123012301230123"))
 
+    // Secret is too short
     XCTAssertNil(encrypted(text: "blah", secret: "deadbeefdeadbeef"))
+
+    // Secret is not valid hex string
     XCTAssertNil(encrypted(text: "blah", secret: "asdfasdfasdfasdfasdfasdfasdfasdf"))
   }
 
   func testDecrypt() {
     XCTAssertEqual("blah", decrypted(text: "af54a6cf18a83a6dc0a38e2895dd1ce4", secret: "DeadBeefDeadBeef0123012301230123"))
 
+    // Secret is too short
     XCTAssertNil(decrypted(text: "836fdf1bf0008e1be7b352d0ccd42dcb", secret: "deadbeefdeadbeef"))
+
+    // Secret is not valid hex string
+    XCTAssertNil(decrypted(text: "836fdf1bf0008e1be7b352d0ccd42dcb", secret: "asdfasdfasdfasdfasdfasdfasdfasdf"))
+
+    // Encrypted text is not even length
     XCTAssertNil(decrypted(text: "8", secret: "deadbeefdeadbeefdeadbeefdeadbeef"))
+
+    // Encrypted text is not valid.
     XCTAssertNil(decrypted(text: "83", secret: "deadbeefdeadbeefdeadbeefdeadbeef"))
+
+    // Encrypted text is not valid hex string.
     XCTAssertNil(decrypted(text: "asdf", secret: "deadbeefdeadbeefdeadbeefdeadbeef"))
   }
 
