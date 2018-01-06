1cd0e807d60cfa6a401c230b78f29616e03041c9
Stephen Celis <stephen.celis@gmail.com>
2018-01-06 17:27:21 -0500

    Plain Text Improvements (#110)
    There's a lot more we can do here, but this is a start!

--- Tests/HtmlTests/PlainTextTests.swift
+++ Tests/HtmlTests/PlainTextTests.swift
@@ -12,13 +12,37 @@ class PlainTextTests: XCTestCase {
           ]),
         body([
           h1(["Welcome!"]),
-          p(["Welcome to you, who has come here"]),
+          p(["Welcome to you & who has come here before."]),
           p([
             "This is a paragraph with ",
             em(["some"]),
-            " inline formatting."
+            " inline formatting and ",
+            a([href("https://example.org")], ["a link"]),
+            "!"
+            ]),
+          p([
+            "Here's an ", strong(["ordered"]), " list:"
+            ]),
+          ol([
+            li(["Preheat oven."]),
+            li(["Mix ingredients."]),
+            li(["Order delivery."])
+            ]),
+          p([
+            "And, \"unordered\":"
+            ]),
+          ul([
+            li(["Milk"]),
+            li(["Eggs"]),
+            li(["Coffee"])
+            ]),
+          p([
+            "They said:"
+            ]),
+          blockquote([
+            "Code to the interface you wish you had, not the interface you were given."
             ])
-          ])
+          ]),
         ])
       ])
 
