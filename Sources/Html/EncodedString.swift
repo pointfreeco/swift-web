28040555e31578d62aeecc4f823d7bbf84d0e2ea
Stephen Celis <stephen@stephencelis.com>
2018-01-06 17:00:03 -0500

    Plain Text Improvements
    There's a lot more we can do here, but this is a start!

--- Sources/Html/EncodedString.swift
+++ Sources/Html/EncodedString.swift
@@ -42,3 +42,13 @@ public func encode(_ unencoded: String) -> EncodedString {
       .replacingOccurrences(of: "'", with: "&#39;")
   )
 }
+
+public func unencode(_ encoded: EncodedString) -> String {
+  return encoded
+    .string
+    .replacingOccurrences(of: "&amp;", with: "&")
+    .replacingOccurrences(of: "&lt;", with: "<")
+    .replacingOccurrences(of: "&gt;", with: ">")
+    .replacingOccurrences(of: "&quot;", with: "\"")
+    .replacingOccurrences(of: "&#39;", with: "'")
+}
--- Sources/Html/PlainText.swift
+++ Sources/Html/PlainText.swift
@@ -1,3 +1,5 @@
+import Prelude
+
 /// Naive transformation of HTML nodes into plain text.
 public func plainText(for nodes: [Node]) -> String {
   return nodes.map(plainText(for:)).joined()
@@ -11,23 +13,91 @@ public func plainText(for node: Node) -> String {
     return ""
   case let .document(document):
     return document.map(plainText).joined()
+      .replacingOccurrences(of: "\n\n+", with: "\n\n", options: .regularExpression)
   case let .element(element):
     return plainText(for: element)
   case let .text(text):
-    return text.string
+    return unencode(text)
   }
 }
 
 private func plainText(for element: Element) -> String {
 
   switch element.name.lowercased() {
+  case "a":
+    return element.content
+      .map(plainText)
+      .map { desc in
+        element.attribs.first(where: { $0.key == "href" })
+          .flatMap { $0.value.renderedValue() }
+          .flatMap(^\.string)
+          .map { "\(desc) <\($0)>" }
+          ?? desc
+      }
+      ?? ""
+  case "b", "strong":
+    return element.content.map(plainText).map { "**\($0)**" } ?? ""
+  case "blockquote":
+    return element.content.map(plainText).map { "> \($0)\n\n" } ?? ""
   case "br":
     return "\n"
-  case "style", "script":
+  case "em", "i":
+    return element.content.map(plainText).map { "_\($0)_" } ?? ""
+  case "h1":
+    return element.content.map(plainText)
+      .map { "\($0)\n\(Array(repeatElement("=", count: $0.count)).joined())\n\n" }
+      ?? ""
+  case "h2":
+    return element.content.map(plainText)
+      .map { "\($0)\n\(Array(repeatElement("-", count: $0.count)).joined())\n\n" }
+      ?? ""
+  case "h3":
+    return element.content.map(plainText).map { "### \($0)\n\n" } ?? ""
+  case "h4":
+    return element.content.map(plainText).map { "#### \($0)\n\n" } ?? ""
+  case "h5":
+    return element.content.map(plainText).map { "##### \($0)\n\n" } ?? ""
+  case "h6":
+    return element.content.map(plainText).map { "###### \($0)\n\n" } ?? ""
+  case "ol":
+    return element.content
+      .map {
+        zip(1..., $0).map { "  \($0). \(plainText(for: $1))" }.joined(separator: "\n") + "\n"
+      }
+      ?? ""
+  case "p":
+    return element.content.map(plainText).map { "\($0)\n\n" } ?? ""
+  case "q":
+    return element.content.map(plainText).map { "\"\($0)\"" } ?? ""
+  case "sub", "sup":
+    return element.content.map(plainText).map { "[\($0)]" } ?? ""
+  case "script", "style":
     return ""
-  case "b", "big", "i", "small", "tt", "abbr", "acronym", "cite", "code", "dfn", "em", "kbd", "strong",
-       "samp", "var", "a", "bdo", "br", "img", "map", "object", "q", "script", "span", "sub", "sup", "button",
-       "input", "label", "select", "textarea":
+  case "ul":
+    return element.content
+      .map { $0.map { "  - \(plainText(for: $0))" }.joined(separator: "\n") + "\n" }
+      ?? ""
+  case "abbr",
+       "acronym",
+       "bdo",
+       "big",
+       "button",
+       "cite",
+       "code",
+       "dfn",
+       "img",
+       "input",
+       "kbd",
+       "label",
+       "map",
+       "object",
+       "samp",
+       "select",
+       "small",
+       "span",
+       "textarea",
+       "tt",
+       "var":
     return (element.content ?? []).map(plainText).joined()
 
   default:
