b9bb943d122e2cfe90ff4eb66a4f0896eda84460
Stephen Celis <stephen@stephencelis.com>
2018-05-27 19:45:28 -0400

    Use en_US_POSIX locale for cookie formatting
    Fixes #135.

--- Sources/HttpPipeline/Response.swift
+++ Sources/HttpPipeline/Response.swift
@@ -142,4 +142,5 @@ public struct Response {
 
 private let expiresDateFormatter = DateFormatter()
   |> \.dateFormat .~ "EEE, d MMM yyyy HH:mm:ss zzz"
+  |> \.locale .~ Locale(identifier: "en_US_POSIX")
   |> \.timeZone .~ TimeZone(abbreviation: "UTC")
