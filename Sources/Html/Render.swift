ce9b5af573d6fd78ea91c6a7943a773b8cb382ab
Stephen Celis <stephen.celis@gmail.com>
2018-07-01 21:50:32 -0400

    Fix pretty renderer for "pre" elements

--- Sources/Html/Render.swift
+++ Sources/Html/Render.swift
@@ -35,8 +35,12 @@ public func render(_ node: Node, config: Config = .compact) -> String {
           $0.map {
             realRender(
               $0,
-              config: config,
-              indentation: indentation + config.indentation
+              config: element.name == "pre"
+                ? Config(indentation: "", newline: config.newline)
+                : config,
+              indentation: element.name == "pre"
+                ? ""
+                : indentation + config.indentation
             )
           }
         }?.joined()
