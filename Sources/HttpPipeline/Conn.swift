58d6ae872d810aedfa1977ef86c5137817f070e9
Mattt <mattt@me.com>
2018-09-14 09:01:38 +0200

    Add default Strict-Transport-Security header

--- Sources/HttpPipeline/Conn.swift
+++ Sources/HttpPipeline/Conn.swift
@@ -17,6 +17,7 @@ public func connection(
   defaultHeaders headers: [Response.Header] = [
 //  .init("Content-Security-Policy", "script-src 'unsafe-inline'; style-src 'unsafe-inline'"),
   .init("Referrer-Policy", "strict-origin-when-cross-origin"),
+  .init("Strict-Transport-Security", "max-age=63072000"),
   .init("X-Content-Type-Options", "nosniff"),
   .init("X-Download-Options", "noopen"),
   .init("X-Frame-Options", "SAMEORIGIN"),
