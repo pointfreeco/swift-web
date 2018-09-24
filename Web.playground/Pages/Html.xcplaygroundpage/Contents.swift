6971ddeab56999344ad5955861c89a41d2031fa6
Stephen Celis <stephen@stephencelis.com>
2018-09-24 01:03:46 -0400

    wip

--- Web.playground/Pages/Html.xcplaygroundpage/Contents.swift
+++ Web.playground/Pages/Html.xcplaygroundpage/Contents.swift
@@ -113,14 +113,20 @@ import HtmlPrettyPrint
 func prettyPrint(_ node: Node, indentation: Int = 2, newline: String = "\n") -> String {
   let indent = { String(repeating: " ", count: indentation) }
 
-  func renderValues(_ values: String, forKey key: String, indentBy: Int) -> String {
+  func renderValues(_ values: String, separator: Character, indentBy: Int) -> String {
+    return values
+      .split(separator: separator)
+      .joined(separator: (separator == " " ? "" : String(separator)) + newline + String(repeating: " ", count: indentBy + (separator == " " ? 1 : 0)))
+  }
+
+  func separator(forKey key: String) -> Character {
     switch key {
     case "class":
-      return values.split(separator: " ").joined(separator: newline + String(repeating: " ", count: indentBy))
+      return " "
     case "style":
-      return values.split(separator: ";").joined(separator: ";" + newline + String(repeating: " ", count: indentBy - 1))
+      return ";"
     default:
-      return values
+      return " "
     }
   }
 
@@ -131,8 +137,8 @@ func prettyPrint(_ node: Node, indentation: Int = 2, newline: String = "\n") -> 
     return indent() + "<!DOCTYPE " + string + ">" + newline
   case let .element(tag, attrs, children):
     let renderedAttrs = attrs.map { k, v -> String in
-      let indentBy = indent().count + tag.count + k.count + 4
-      return " " + k + (v.map { "=\"\(renderValues($0, forKey: k, indentBy: indentBy))\"" } ?? "")
+      let indentBy = indent().count + tag.count + k.count + 3
+      return " " + k + (v.map { "=\"\(renderValues($0, separator: separator(forKey: k), indentBy: indentBy))\"" } ?? "")
       }.joined(separator: newline + indent() + String(repeating: " ", count: tag.count + 1))
     guard !voidElements.contains(tag) else {
       return indent() + "<" + tag + renderedAttrs + ">" + newline
