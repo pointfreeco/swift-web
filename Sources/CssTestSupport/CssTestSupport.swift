9023be3a6ff574dc1ddd1b68254be5af9cc4386b
Brandon Williams <mbw234@gmail.com>
2018-11-04 16:06:37 -0500

    Update swift-snapshot-testing. (#165)
    * Update swift-snapshot-testing.
    
    * wip

--- Sources/CssTestSupport/CssTestSupport.swift
+++ Sources/CssTestSupport/CssTestSupport.swift
@@ -1,9 +1,9 @@
 import Css
 import SnapshotTesting
 
-extension Strategy {
+extension Strategy where A == Stylesheet, B == String {
   public static func css(_ config: Config) -> Strategy<Stylesheet, String> {
-    var css = Strategy.lines.pullback { (stylesheet: Stylesheet) in
+    var css = SimpleStrategy.lines.pullback { (stylesheet: Stylesheet) in
       render(config: config, css: stylesheet)
     }
     css.pathExtension = "css"
