d80f881b2b35859de25cc73dcd36d71bec2cdf92
Stephen Celis <stephen@stephencelis.com>
2018-11-07 19:25:51 -0500

    Bump

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .library(name: "View", targets: ["View"])
     ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("052f3ae")),
+    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("f270907")),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("afb1918")),
     .package(url: "https://github.com/pointfreeco/swift-html", .revision("0.1.3")),
     .package(url: "https://github.com/apple/swift-nio.git", from: "1.8.0"),
