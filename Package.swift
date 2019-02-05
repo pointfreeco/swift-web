5dbbbf1659c60671a1b09cde1f3bc729f2682801
Brandon Williams <mbw234@gmail.com>
2019-02-04 20:01:55 -0500

    Update swift-html

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("8cbc934")),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/swift-html", from: "0.2.0"),
+    .package(url: "https://github.com/pointfreeco/swift-html", from: "0.2.1"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "1.8.0"),
     .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", .exact("1.0.4")),
   ],
