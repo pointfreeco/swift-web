8dc54e616843616c13ede727f38b302f1efa6bda
Stephen Celis <stephen@stephencelis.com>
2019-03-26 14:57:18 -0400

    bump

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-prelude.git", .branch("swift-5")),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-html", from: "0.2.1"),
     .package(url: "https://github.com/apple/swift-nio.git", .branch("master")),
     .package(url: "https://github.com/apple/swift-nio-extras.git", .revision("3c1bf78")),
