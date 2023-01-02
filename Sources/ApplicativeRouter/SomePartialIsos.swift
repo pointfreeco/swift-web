a74fe1b45d3f1d22884c43eab59984d0edf59df2
Brandon Williams <mbrandonw@hey.com>
2023-01-01 17:48:23 -0800

    wip

--- Sources/ApplicativeRouter/SomePartialIsos.swift
+++ Sources/ApplicativeRouter/SomePartialIsos.swift
@@ -10,7 +10,7 @@ extension Optional {
     public static var some: PartialIso<Wrapped, Wrapped?> {
       return .init(
         apply: Optional.some,
-        unapply: id
+        unapply: Prelude.id
       )
     }
   }
