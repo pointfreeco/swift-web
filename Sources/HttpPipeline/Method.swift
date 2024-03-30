c77329c52bfc16ea428ff12a77aebedf26aa504e
alephao <7674479+alephao@users.noreply.github.com>
2024-03-30 13:22:29 -0300

    refactor: use swift-http-types method instead of own Method

--- Sources/HttpPipeline/Method.swift
+++ Sources/HttpPipeline/Method.swift
@@ -1,28 +1,3 @@
-public enum Method {
-  case get
-  case post
-  case put
-  case patch
-  case delete
-  case options
-  case other(String)
+import HTTPTypes
 
-  public var description: String {
-    switch self {
-    case .get:
-      return "GET"
-    case .post:
-      return "POST"
-    case .put:
-      return "PUT"
-    case .patch:
-      return "PATCH"
-    case .delete:
-      return "DELETE"
-    case .options:
-      return "OPTIONS"
-    case let .other(str):
-      return str
-    }
-  }
-}
+public typealias Method = HTTPRequest.Method
