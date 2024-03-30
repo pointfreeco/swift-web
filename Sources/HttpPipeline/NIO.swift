010a2017612a375f913a6bf32314bf02bbefc419
alephao <7674479+alephao@users.noreply.github.com>
2024-03-30 13:41:57 -0300

    refactor: remove redundant code for getting method raw value

--- Sources/HttpPipeline/NIO.swift
+++ Sources/HttpPipeline/NIO.swift
@@ -128,41 +128,5 @@ private final class Handler: ChannelInboundHandler {
 }
 
 private func method(from method: HTTPMethod) -> String {
-  switch method {
-  case .GET: return "GET"
-  case .PUT: return "PUT"
-  case .ACL: return "ACL"
-  case .HEAD: return "HEAD"
-  case .POST: return "POST"
-  case .COPY: return "COPY"
-  case .LOCK: return "LOCK"
-  case .MOVE: return "MOVE"
-  case .BIND: return "BIND"
-  case .LINK: return "LINK"
-  case .PATCH: return "PATCH"
-  case .TRACE: return "TRACE"
-  case .MKCOL: return "MKCOL"
-  case .MERGE: return "MERGE"
-  case .PURGE: return "PURGE"
-  case .NOTIFY: return "NOTIFY"
-  case .SEARCH: return "SEARCH"
-  case .UNLOCK: return "UNLOCK"
-  case .REBIND: return "REBIND"
-  case .UNBIND: return "UNBIND"
-  case .REPORT: return "REPORT"
-  case .DELETE: return "DELETE"
-  case .UNLINK: return "UNLINK"
-  case .CONNECT: return "CONNECT"
-  case .MSEARCH: return "MSEARCH"
-  case .OPTIONS: return "OPTIONS"
-  case .PROPFIND: return "PROPFIND"
-  case .CHECKOUT: return "CHECKOUT"
-  case .PROPPATCH: return "PROPPATCH"
-  case .SUBSCRIBE: return "SUBSCRIBE"
-  case .MKCALENDAR: return "MKCALENDAR"
-  case .MKACTIVITY: return "MKACTIVITY"
-  case .UNSUBSCRIBE: return "UNSUBSCRIBE"
-  case .SOURCE: return "SOURCE"
-  case let .RAW(value): return value
-  }
+  return method.rawValue
 }
